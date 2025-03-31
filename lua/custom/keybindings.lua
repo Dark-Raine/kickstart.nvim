0755ea552554402b4de7d060f41285e4bd4cb620
Anderson Coelho <axzonexgamer@gmail.com>
2025-03-31 22:00:13 +0100

    feat: add various useful keybinds

--- lua/custom/keybindings.lua
+++ lua/custom/keybindings.lua
@@ -1,1 +1,8 @@
--- local map = vim.keymap.set
+-- local map = vim.keymap.setting
+-- exit insert mode
+vim.keymap.set('i', 'jj', '<Esc>', { noremap = true, silent = true })
+-- cycle through buffers/tabs
+vim.keymap.set('n', '<Tab>', '<cmd>BufferLineCycleNext<CR>', { desc = 'Next buffer' })
+vim.keymap.set('n', '<S-Tab>', '<cmd>BufferLineCyclePrev<CR>', { desc = 'Previous buffer' })
+-- close current buffer
+vim.keymap.set('n', '<leader>bx', '<cmd>bdelete<CR>', { desc = 'Close current buffer' })
