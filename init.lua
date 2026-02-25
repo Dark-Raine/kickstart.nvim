80b21f415eadbd1aadd6539401315f7d67be07c0
Anderson Coelho <andersonc@chillibean.com>
2026-02-25 09:29:28 +0000

    feat: added code review and terraform support

--- init.lua
+++ init.lua
@@ -208,6 +208,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- set terraform filetype --
+vim.filetype.add {
+  extension = {
+    tf = 'terraform',
+    tfvars = 'terraform',
+  },
+}
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
